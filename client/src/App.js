a63647223157211725f7c3786c8ae3bb364b186d
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2022-11-04 19:02:45 +0200

    frontend init (closes #31)

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import "bulma/css/bulma.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">Empathist</div>
+    </div>
+  )
+}
+
+export default App
