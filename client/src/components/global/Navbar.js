00b961a53508d34073241278bf5088585657ed96
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2022-11-10 00:03:50 +0200

    added navigation buttons, navbar is responsive (closes #9)

--- client/src/components/global/Navbar.js
+++ client/src/components/global/Navbar.js
@@ -1,34 +1,64 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+  const [navToggle, setNavToggle] = useState("")
+  const [navMenu, setNavMenu] = useState("")
+
+  const handleNavToggle = () => {
+    navToggle === "" ? setNavToggle("is-active") : setNavToggle("")
+    navMenu === "" ? setNavMenu("is-active") : setNavMenu("")
+  }
+
   return (
     <>
-      <nav className="navbar is-dark is-spaced" role="navigation">
+      <nav className="navbar is-dark" role="navigation">
         <div className="navbar-brand">
           <div
-            className=" navbar-item title is-size-3 mr-6 has-text-light"
+            className=" navbar-item is-size-2 is-size-4-touch ml-3 mr-6 has-text-light"
             id="navTitle"
           >
             Empathist
           </div>
+          <div
+            role="button"
+            className={`navbar-burger ${navToggle}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={handleNavToggle}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
         </div>
-        <div id="navbar" className="navbar-menu is-size-4">
+        <div
+          id="navbar"
+          className={`navbar-menu is-size-4 has-background-dark ${navMenu}`}
+        >
           <div className="navbar-start">
-            <Link className="navbar-item" to="/">
+            <Link id="navItem" className="navbar-item has-text-light" to="/">
               Welcome
             </Link>
-            <Link className="navbar-item" to="findChat">
+            <Link className="navbar-item has-text-light" to="findChat">
               Find chat
             </Link>
-            <Link className="navbar-item" to="chat">
+            <Link className="navbar-item has-text-light" to="chat">
               Chat
             </Link>
           </div>
           <div className="navbar-end">
-            <Link className="navbar-item is-size-4 mx-3" to="login">
+            <Link
+              className="navbar-item is-size-4 mx-3 has-text-light has-text-light"
+              to="login"
+            >
               Login
             </Link>
-            <Link className="navbar-item is-size-4 mx-3" to="signup">
+            <Link
+              className="navbar-item is-size-4 mx-3 has-text-light"
+              to="signup"
+            >
               Sign up
             </Link>
           </div>
