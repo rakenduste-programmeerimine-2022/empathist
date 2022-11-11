1983bf945ab164eb595725edfee9f0153ecdf119
Mares1492 <msumarok@tlu.ee>
2022-11-11 19:44:39 +0200

    index.js is configured (closes #13 )
    - connection to db is established

--- server/index.js
+++ server/index.js
@@ -1,12 +1,39 @@
 const express = require('express')
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express()
 const WSServer = require('express-ws')(app)
 require('dotenv').config()
+//const user = require('./routes/user-routes')
+//const chat = require('./routes/chat-routes')
+
 
 const PORT = process.env.PORT || 5000
 
-// app.ws('/',(ws,req)=>{
-//
-// })
+app.use(morgan('dev'))
+app.use(cors({
+    credentials:true,
+    origin: process.env.CLIENT_URL
+}))
+
+//app.use('/user',user)
+//app.use('/chat',chat)
+
+app.get('*', (req, res) => {
+    res.send(`<h1>There's definitely something interesting around here, just keep looking...</h1>`)
+})
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@empathist.epepriy.mongodb.net/?retryWrites=true&w=majority`
+const connectToDB = async () => {
+    try{
+        await mongoose.connect(uri)
+        console.log('Database connection established')
+    }catch (err){
+        console.log(err)
+    }
+}
+
+connectToDB()
 
 app.listen(PORT,()=>console.log(`Server started on PORT: ${PORT}`))
