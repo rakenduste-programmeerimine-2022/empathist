22b00eff7a50e6679f89bfe67b35120df4d41ae0
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2022-11-15 23:31:27 +0200

    user authentication FE (closes #15)

--- client/src/components/global/Login.js
+++ client/src/components/global/Login.js
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
-const Login = () => {
+const Login = ({ isActive, setIsActive }) => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, password)
+  }
   return (
     <>
-      <div className="modal is-active">
+      <div className={isActive ? "modal is-active" : "modal"}>
         <div className="modal-background"></div>
         <div className="modal-card px-2">
           <header className="modal-card-head">
             <p className="modal-card-title is-size-3">Log in</p>
-            <button className="delete" aria-label="close"></button>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => setIsActive(false)}
+            ></button>
           </header>
           <section className="modal-card-body">
-            <form>
+            <form onSubmit={handleFormSubmit}>
               <div className="field">
                 <label className="label is-size-5">Username</label>
                 <div className="control">
@@ -19,6 +31,8 @@ const Login = () => {
                     className="input"
                     type="text"
                     placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                   ></input>
                 </div>
               </div>
@@ -29,12 +43,15 @@ const Login = () => {
                     className="input"
                     type="password"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   ></input>
                 </div>
               </div>
               <div className="field">
                 <p>
-                  Don't have an account? <Link to="/signup">Sign up</Link>
+                  Don't have an account?
+                  <Link>Sign up</Link>
                 </p>
               </div>
               <div className="field is-grouped">
@@ -42,7 +59,12 @@ const Login = () => {
                   <button className="button is-link">Submit</button>
                 </p>
                 <p className="control">
-                  <button className="button is-link is-light">Cancel</button>
+                  <button
+                    className="button is-link is-light"
+                    onClick={() => setIsActive(false)}
+                  >
+                    Cancel
+                  </button>
                 </p>
               </div>
             </form>
--- client/src/components/global/Signup.js
+++ client/src/components/global/Signup.js
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
-const Signup = () => {
+const Signup = ({ isActive, setIsActive }) => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, password, email)
+  }
   return (
     <>
-      <div className="modal is-active">
+      <div className={isActive ? "modal is-active" : "modal"}>
         <div className="modal-background"></div>
         <div className="modal-card px-2">
           <header className="modal-card-head">
             <p className="modal-card-title is-size-3">Sign up</p>
-            <button className="delete" aria-label="close"></button>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => setIsActive(false)}
+            ></button>
           </header>
           <section className="modal-card-body">
-            <form>
+            <form onSubmit={handleFormSubmit}>
               <div className="field">
                 <label className="label is-size-5">Username</label>
                 <div className="control">
@@ -19,6 +32,8 @@ const Signup = () => {
                     className="input"
                     type="text"
                     placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                   ></input>
                 </div>
               </div>
@@ -29,6 +44,8 @@ const Signup = () => {
                     className="input"
                     type="email"
                     placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   ></input>
                 </div>
               </div>
@@ -39,20 +56,29 @@ const Signup = () => {
                     className="input"
                     type="password"
                     placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   ></input>
                 </div>
               </div>
               <div className="field">
                 <p>
-                  Already have an account? <Link to="/login">Login</Link>
+                  Already have an account? <Link>Login</Link>
                 </p>
               </div>
               <div className="field is-grouped">
                 <p className="control">
-                  <button className="button is-link">Submit</button>
+                  <button className="button is-link" type="submit">
+                    Submit
+                  </button>
                 </p>
                 <p className="control">
-                  <button className="button is-link is-light">Cancel</button>
+                  <button
+                    className="button is-link is-light"
+                    onClick={() => setIsActive(false)}
+                  >
+                    Cancel
+                  </button>
                 </p>
               </div>
             </form>
