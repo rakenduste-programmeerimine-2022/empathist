6b0ac9a0c71de30ccd05e99d07640393959cce94
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2022-11-21 20:12:57 +0200

    Find chat page setup FE (closes #23)

--- client/src/pages/chat/FindChat.js
+++ client/src/pages/chat/FindChat.js
@@ -1,8 +1,86 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+
+const searchOptions = [{ name: "By name" }]
+const chatList = [
+  {
+    chatroomName: "Chat room 1",
+  },
+  {
+    chatroomName: "Chat room 2",
+  },
+  {
+    chatroomName: "Chat room 3",
+  },
+  {
+    chatroomName: "Chat room 4",
+  },
+  {
+    chatroomName: "Chat room 5",
+  },
+  {
+    chatroomName: "Chat room 6",
+  },
+  {
+    chatroomName: "Chat room 7",
+  },
+]
+
 const FindChat = () => {
+  const [searchRequest, setSearchRequest] = useState("")
+  console.log(searchRequest)
+
   return (
-    <div>
-      <div className="title">Find chat</div>
-    </div>
+    <>
+      <section className="hero-head">
+        <div className="title">Find chat</div>
+        <div className="search-bar py-3">
+          <div className="dropdown is-hoverable">
+            <div className="dropdown-trigger">
+              <button
+                className="button"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+              >
+                <span>Search options</span>
+                <span className="icon is-small">
+                  <FontAwesomeIcon icon={faAngleDown} />
+                </span>
+              </button>
+              <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  {searchOptions.map((option) => (
+                    <Link className="dropdown-item">{option.name}</Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Search"
+                onChange={(e) => setSearchRequest(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="hero-body p-2 mt-3 chat-list">
+        {chatList.map((chatRoom) => (
+          <div className="notification chat-item">
+            <p className="is-size-2 has-text-weight-medium">
+              {chatRoom.chatroomName}
+            </p>
+            <div className="button is-info px-5 is-size-5">Join</div>
+          </div>
+        ))}
+      </section>
+    </>
   )
 }
 export default FindChat
